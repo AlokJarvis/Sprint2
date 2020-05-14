f91fe253e55169f883bcffeb4bf8451f6159598a
AlokJarvis <60642135+AlokJarvis@users.noreply.github.com>
2020-05-15 03:27:02 +0530

    commit2

--- AdminSystem/src/main/java/com/capg/hcs/admin/AdminSystemApplication.java
+++ AdminSystem/src/main/java/com/capg/hcs/admin/AdminSystemApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class AdminSystemApplication {
 
 	public static void main(String[] args) {
